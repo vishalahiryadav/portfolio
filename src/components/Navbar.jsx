f15460a74b61c0dd87cc129aad214c8281d5d3de
Vishal Ahir <vishalahir8002@gmail.com>
2024-09-03 10:12:36 +0530

    initial commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import {useEffect ,useState} from 'react'
+import { Link } from 'react-router-dom'
+import {logo , menu , close} from '../assets' 
+import {styles} from '../styles'
+import {navLinks} from '../constants'
+
+function Navbar() {
+  return (
+    <nav
+    className={`${styles.paddingX} w-full flex items-center py-5`}>
+
+    </nav>
+  )
+}
+
+export default Navbar
