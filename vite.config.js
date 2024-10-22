7953b8d32331e69a4a7857da5d5b2239697cc2a8
Vishal Ahir <vishalahir8002@gmail.com>
2024-10-22 18:05:00 +0530

    again changing path

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/vishal-portfolio/', // Replace with your repo name
+  base: './', // Replace with your repo name
   server: {
     host: true, // Allow access from the local network
     port: 5173, // You can change this to any port you prefer
